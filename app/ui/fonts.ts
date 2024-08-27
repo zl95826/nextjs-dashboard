df713fc342f37f2bba8bee934fa30963ee9ca5c6
zl95826 <zl95826@hotmail.com>
2024-08-26 20:50:47 -0700

    add layout, pictures, font and styles

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Roboto_Flex } from "next/font/google";
+export const inter = Inter({ subsets: ["latin"] });
+export const roboto = Roboto_Flex({
+  subsets: ["latin"],
+  weight: ["100", "400", "700"],
+});
