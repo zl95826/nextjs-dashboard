192d2254d48edaf6832fd4f6d9065cef31d50ec2
zl95826 <zl95826@hotmail.com>
2024-09-03 22:09:28 -0700

    add active links

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,11 @@
+"use client";
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -18,6 +20,8 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <>
       {links.map((link) => {
